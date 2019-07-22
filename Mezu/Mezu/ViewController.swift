def5069ff2683c3457cd2715c8d43fc851529316
Artem Umanets <artem.u.v@gmail.com>
2019-07-22 12:50:44 +0100

    Empty project commit

---
+++ Mezu/Mezu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mezu
+//
+//  Created by Artem Umanets on 22/07/2019.
+//  Copyright © 2019 Artem Umanets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
